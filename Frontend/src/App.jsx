740770c09978b7af1a522392cdcf554d76449621
Rahul15205 <rahulkumarshukla4627@gmail.com>
2025-01-15 16:01:38 +0530

    Co-authored-by: Rohit Srivastava <rohit2215@users.noreply.github.com>

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+
 
 const App = () => {
   return (
-    <div className="text-green-500">
-      testing 
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path="/" element = {<Home />} />
+      </Routes>
     </div>
   );
 };
