9d4b9759ab6b5fa0d96e3a9ea788e58ed6df071f
Rahul15205 <rahulkumarshukla4627@gmail.com>
2025-01-15 11:36:01 +0530

    configuration

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
   return (
-    <div className='text-green-500'>
-      Testing Tailwind
+    <div className="text-green-500">
+      testing 
     </div>
   );
 };
