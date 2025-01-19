858a50de8710e39fc3c0581323b2d95fc47b4f7e
Rahul15205 <rahulkumarshukla4627@gmail.com>
2025-01-19 15:47:04 +0530

    home page creation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Faculty from "./pages/Faculty";
 import Login from "./pages/Login";
 import About from "./pages/About";
