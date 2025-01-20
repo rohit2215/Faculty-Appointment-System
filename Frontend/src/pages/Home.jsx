89a903a32f690b60690b19b81c2dbb4dc8a960f3
Rohit Srivastava <rohitsrivastava10156@gmail.com>
2025-01-20 21:43:55 +0530

    Added new feature

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../componenets/Header'
 import SpecialityMenu from '../componenets/SpecialityMenu'
+import TopDoctors from '../componenets/TopDoctors'
 
 const Home = () => {
   return (
     <div>
       <Header />
       <SpecialityMenu />
+      <TopDoctors/> 
     </div>
   )
 }
