fee237d3494069a851837af61417dfff16d0a9dc
Rahul15205 <rahulkumarshukla4627@gmail.com>
2025-01-22 10:40:30 +0530

    fully completed appointment page next about

--- Frontend/src/componenets/RelatedDoctors.jsx
+++ Frontend/src/componenets/RelatedDoctors.jsx
@@ -1,16 +1,23 @@
-import React, { useContext } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../context/AppContext'
+import { useNavigate } from 'react-router-dom'
 
-const TopDoctors = () => {
-  const navigate = useNavigate()
-  const {doctors} = useContext(AppContext);
+const RelatedDoctors = ({speciality,facId}) => {
+    const {doctors} = useContext(AppContext)
+    const navigate = useNavigate()
+    const [relDoc,setRelDoc] = useState([])
+    useEffect(()=>{
+        if(doctors.length > 0 && speciality){
+            const doctorsData = doctors.filter((doc) => doc.speciality === speciality && doc._id !== facId)
+            setRelDoc(doctorsData)
+        }
+    },[doctors,speciality,facId])
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
         <h1 className='text-3xl font-medium'>Top Faculties to Consult</h1>
         <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted Faculties, schedule your appointment hassle-free. </p>
         <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0 '>
-           {doctors.slice(0,10).map((item,index)=>(
+           {relDoc.slice(0,5).map((item,index)=>(
             <div onClick={()=>{navigate(`/appointment/${item._id}`); scrollTo(0,0)}} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transation-all duration-500' key={index}>
                 <img className='bg-blue-50' src={item.image} alt="" />
                 <div className='p-4'>
@@ -29,4 +36,4 @@ const TopDoctors = () => {
   )
 }
 
-export default TopDoctors
+export default RelatedDoctors
