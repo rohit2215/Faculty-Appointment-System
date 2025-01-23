0e7e04f36bb68788d7839b1760499452da678e91
Rahul15205 <rahulkumarshukla4627@gmail.com>
2025-01-23 22:06:34 +0530

    login page created fully next my-profile page

--- Frontend/src/pages/Appointment.jsx
+++ Frontend/src/pages/Appointment.jsx
@@ -62,7 +62,7 @@ const Appointment = () => {
   },[docInfo])
 
   useEffect(() => {
-    console.log(docSlots);
+    
   },[docSlots])
 
   return docInfo && (
--- Frontend/src/pages/Contact.jsx
+++ Frontend/src/pages/Contact.jsx
@@ -11,13 +11,13 @@ const Contact = () => {
       </div>
        <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
         <img className='w-full md:max-w-[360px]'src={assets.contact_image} alt="" />
-        <div className='flex flex-col justify-center item-start-gap-6'>
-          <p>OUR OFFICE</p>
-          <p>SEC-11,Indira Nagar, <br />Lucknow,226016</p>
-          <p>Tel: (000) 000-0000 <br />Email: greatstackdev@gmail.com</p>
-          <p>CAREERS AT AmiPal</p>
-          <p>Learn more about our teams and job openings.</p>
-          <button>Explore Jobs</button>
+        <div className='flex flex-col justify-center item-start gap-6'>
+          <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
+          <p className='text-gray-500'>SEC-11,Indira Nagar, <br />Lucknow,226016</p>
+          <p className='text-gray-500'>Tel: (000) 000-0000 <br />Email: greatstackdev@gmail.com</p>
+          <p className='font-semibold text-lg text-gray-600'>CAREERS AT AmiPal</p>
+          <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
         </div>
        </div>
     </div>
