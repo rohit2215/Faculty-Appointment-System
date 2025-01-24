a78eaede3f6aabd4f5e3407d3f8f438d39f0381e
Rahul15205 <rahulkumarshukla4627@gmail.com>
2025-01-24 20:48:14 +0530

    frontend completed

--- Frontend/src/componenets/NavBar.jsx
+++ Frontend/src/componenets/NavBar.jsx
@@ -61,7 +61,7 @@ const NavBar = () => {
           </div>
           <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
             <NavLink  onClick={()=>setShowMenu(false)} to='/'><p className='px-4 py-2 rounded inline-block'>HOME</p></NavLink>
-            <NavLink  onClick={()=>setShowMenu(false)} to='/doctors'><p className='px-4 py-2 rounded inline-block'>ALL DOCTORS</p></NavLink>
+            <NavLink  onClick={()=>setShowMenu(false)} to='/faculties'><p className='px-4 py-2 rounded inline-block'>ALL DOCTORS</p></NavLink>
             <NavLink  onClick={()=>setShowMenu(false)} to='/about'><p className='px-4 py-2 rounded inline-block'>ABOUT</p></NavLink>
             <NavLink  onClick={()=>setShowMenu(false)} to='/contact'><p className='px-4 py-2 rounded inline-block'>CONTACT</p></NavLink>
           </ul>
--- Frontend/src/pages/Faculty.jsx
+++ Frontend/src/pages/Faculty.jsx
@@ -21,8 +21,8 @@ const Faculty = () => {
     <div>
       <p className='text-gray-600'>Browse through the Faculties based on their specialities.</p>
       <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
-        <button>Filters</button>
-        <div className='flex flex-col gap-4 text-sm text-gray-600'>
+        <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-primary':''}`} onClick={() => setShowFilter(prev => !prev)}>Filters</button>
+        <div className={`flex flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex':'hidden sm:block'}`}>
          <p onClick={()=> speciality === 'General physician' ? navigate('/faculties'):navigate('/faculties/General physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "General physician" ? "bg-indigo-100 text-black" : ""}`}>Career Counseling Specialist</p>
          <p onClick={()=> speciality === 'Gynecologist' ? navigate('/faculties'):navigate('/faculties/Gynecologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Gynecologist" ? "bg-indigo-100 text-black" : ""}`}>Mental Health Counselor</p>
          <p onClick={()=> speciality === 'Dermatologist' ? navigate('/faculties'):navigate('/faculties/Dermatologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Dermatologist" ? "bg-indigo-100 text-black" : ""}`}>Academic Counselor</p>
