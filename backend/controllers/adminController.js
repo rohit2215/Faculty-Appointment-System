a7e0450ab483d8112d2751296aef0e4b91587e62
Rohit Srivastava <rohitsrivastava10156@gmail.com>
2025-02-04 00:28:53 +0530

    started Display & Manage Appointment for Admin

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -3,6 +3,7 @@ import validator from 'validator'
 import { v2 as cloudinary } from 'cloudinary'
 import doctorModel from '../models/doctorModel.js'
 import jwt from 'jsonwebtoken'
+import appointmentModel from '../models/appointmentModel.js'
 
 // API for adding doctor
 const addDoctor = async (req,res) => {
@@ -83,5 +84,15 @@ const allDoctors = async (req,res) => {
         res.json({success: false, message: error.message})
     }
 }
+//API to get all appointments list
+const appointmentsAdmin = async (req,res)=> {
+    try{
+        const appointments=await appointmentModel.find({})
+        res.json({success:true,appointments})
+    } catch(error){
+        res.json({success:false,message:error.message})
+        
+    }
 
-export {addDoctor , loginAdmin, allDoctors}
+}
+export {addDoctor , loginAdmin, allDoctors,appointmentsAdmin}
