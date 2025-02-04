b0c7ad6ffd7b632394b124d1aa2b231c443c9b95
Rahul15205 <rahulkumarshukla4627@gmail.com>
2025-02-04 21:06:11 +0530

    Appointments page in admin panel completed

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -7,6 +7,8 @@ export const AdminContext = createContext()
 const AdminContextProvider = (props) => {
     const [aToken, setAToken] = useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
     const [doctors, setDoctors] = useState([])
+    const [appointments,setAppointments] = useState([])
+
     const backendUrl = import.meta.env.VITE_BACKEND_URL
     const getAllDoctors = async () => {
         try {
@@ -35,10 +37,56 @@ const AdminContextProvider = (props) => {
             toast.error(error.message)
         }
     }
+    const getAllAppointments = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + '/api/admin/appointments',{headers:{aToken}})
+            if (data.success) {
+                setAppointments(data.appointments)
+                console.log(data.appointments);
+                
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    const cancelAppointment = async (appointmentId) => {
+        try {
+            const {data} = await axios.post(backendUrl + '/api/admin/cancel-appointment',{appointmentId},{headers:{aToken}})
+            console.log(data);
+            if(data.success){
+                toast.success(data.message)
+                getAllAppointments()
+            }else{
+                toast.error(data.message)
+                console.log(data.message);
+                
+            }
+        } catch (error) {
+            if (error.response) {
+                // The request was made, but the server responded with a status code outside the range of 2xx
+                console.log("Server Error:", error.response.data);
+                toast.error(error.response.data.message || "Something went wrong");
+            } else if (error.request) {
+                // The request was made but no response was received
+                console.log("Network Error:", error.request);
+                toast.error("Network error, please try again");
+            } else {
+                // Something happened while setting up the request
+                console.log("Axios Error:", error.message);
+                toast.error(error.message);
+            }
+        }
+    }
     const value = {
         aToken, setAToken,
         backendUrl,doctors,
-        getAllDoctors,changeAvailability
+        getAllDoctors,changeAvailability,
+        appointments,setAppointments,
+        getAllAppointments,
+        cancelAppointment
     }
     return (
         <AdminContext.Provider value={value}>
--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,98 +1,155 @@
-import bcrypt from 'bcrypt'
-import validator from 'validator'
-import { v2 as cloudinary } from 'cloudinary'
-import doctorModel from '../models/doctorModel.js'
-import jwt from 'jsonwebtoken'
-import appointmentModel from '../models/appointmentModel.js'
+import bcrypt from "bcrypt";
+import validator from "validator";
+import { v2 as cloudinary } from "cloudinary";
+import doctorModel from "../models/doctorModel.js";
+import jwt from "jsonwebtoken";
+import appointmentModel from "../models/appointmentModel.js";
 
 // API for adding doctor
-const addDoctor = async (req,res) => {
-    try {
-        const {name, email, password, speciality, degree, experience, about, fees, address} = req.body
-        const imageFile = req.file
+const addDoctor = async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address,
+    } = req.body;
+    const imageFile = req.file;
 
-        // checking for all data to add doctor
-        if(!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address) {
-            return res.json({success: false, message: 'Missing Details'})
-        }
-
-        // validating email format
-        if(!validator.isEmail(email)){
-            return res.json({success: false, message: 'Please enter a valid email'})
-        }
-
-        // validating password
-        if(password.length < 8) {
-            return res.json({success: false, message: 'Password should be atleast 8 characters'})
-        }
-
-        // hashing password
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password, salt)
-
-        // uploading image to cloudinary
-        const imageUpload = await cloudinary.uploader.upload(imageFile.path,{resource_type: 'image'})
-        const imageUrl = imageUpload.secure_url
-
-        const doctorData = {
-            name,
-            email,
-            image: imageUrl,
-            password: hashedPassword,
-            speciality,
-            degree,
-            experience,
-            about,
-            fees,
-            address: JSON.parse(address),
-            date: Date.now()
-        }
-
-        const newDoctor = new doctorModel(doctorData)
-        await newDoctor.save()
-
-        res.json({success: true, message: 'Doctor added successfully'})
-    }catch (error) {
-        console.error(error)
-        res.json({success: false, message: error.message})
+    // checking for all data to add doctor
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !speciality ||
+      !degree ||
+      !experience ||
+      !about ||
+      !fees ||
+      !address
+    ) {
+      return res.json({ success: false, message: "Missing Details" });
     }
-}
+
+    // validating email format
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
+
+    // validating password
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Password should be atleast 8 characters",
+      });
+    }
+
+    // hashing password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // uploading image to cloudinary
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: "image",
+    });
+    const imageUrl = imageUpload.secure_url;
+
+    const doctorData = {
+      name,
+      email,
+      image: imageUrl,
+      password: hashedPassword,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address: JSON.parse(address),
+      date: Date.now(),
+    };
+
+    const newDoctor = new doctorModel(doctorData);
+    await newDoctor.save();
+
+    res.json({ success: true, message: "Doctor added successfully" });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // API for admin Login
 
-const loginAdmin = async (req,res) => {
-    try {
-        const {email,password} = req.body
+const loginAdmin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
-            const token = jwt.sign(email+password, process.env.JWT_SECRET)
-            res.json({success: true, token})
-        }else{
-            res.json({success: false, message: 'Invalid Credentials'})
-        }
-    } catch (error) {
-        res.json({success: false, message: error.message})  
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid Credentials" });
     }
-}
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 
 //API to get all doctors list for admin panel
-const allDoctors = async (req,res) => {
-    try {
-        const doctors = await doctorModel.find({}).select('-password')
-        res.json({success: true, doctors})
-    } catch (error) {
-        res.json({success: false, message: error.message})
-    }
-}
+const allDoctors = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({}).select("-password");
+    res.json({ success: true, doctors });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 //API to get all appointments list
-const appointmentsAdmin = async (req,res)=> {
-    try{
-        const appointments=await appointmentModel.find({})
-        res.json({success:true,appointments})
-    } catch(error){
-        res.json({success:false,message:error.message})
-        
-    }
+const appointmentsAdmin = async (req, res) => {
+  try {
+    const appointments = await appointmentModel.find({});
+    res.json({ success: true, appointments });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 
-}
-export {addDoctor , loginAdmin, allDoctors,appointmentsAdmin}
+//API for appointment cancellation
+const appointmentCancel = async (req, res) => {
+  try {
+    const { appointmentId } = req.body;
+    const appointmentData = await appointmentModel.findById(appointmentId);
+    
+    await appointmentModel.findByIdAndUpdate(appointmentId, {
+      cancelled: true,
+    });
+
+    //releasing doctor slot
+
+    const { facId, slotDate, slotTime } = appointmentData;
+    const doctorData = await doctorModel.findById(facId);
+    let slots_booked = doctorData.slots_booked;
+    slots_booked[slotDate] = slots_booked[slotDate].filter(
+      (e) => e !== slotTime
+    );
+
+    await doctorModel.findByIdAndUpdate(facId, { slots_booked });
+
+    res.json({ success: true, message: "Appointment cancelled" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+export { addDoctor, loginAdmin, allDoctors, appointmentsAdmin,appointmentCancel };
