5ce77fda1660e3fdce05bf86c77c13acb5f52d36
Rahul15205 <rahulkumarshukla4627@gmail.com>
2025-02-10 20:25:46 +0530

    Remove unused useEffect for token navigation in Login component

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -37,11 +37,11 @@ const Login = () => {
     }
   }
 
-  useEffect(() => {
-    if (token) {
-      navigate('/')
-    }
-  },[token])
+  // useEffect(() => {
+  //   if (token) {
+  //     navigate('/')
+  //   }
+  // },[token])
   return (
     <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
       <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
